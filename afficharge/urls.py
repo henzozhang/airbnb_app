e4477d622272b60aec5ace5a4a40d4158b5675db
léon zhang <zhangleon1985@gmail.com>
2022-11-30 11:40:23 +0100

    leon1 ajouter requirements

--- afficharge/urls.py
+++ afficharge/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('', views.home_page, name='home')
     # path('about/', views.about_view, name='about'),
     # path('bordeaux/', views.bordeaux, name='bordeaux'),
     # path('lyon/', views.lyon,  name='lyon'),
